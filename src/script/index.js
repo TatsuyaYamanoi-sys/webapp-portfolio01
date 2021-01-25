b957ab8f6a536d22e4febb03cdd05c0e04f972dd
Tatsuya Yamanoi <71238768+TatsuyaYamanoi-sys@users.noreply.github.com>
2021-01-26 02:22:53 +0900

    未完了エリアに削除・完了機能を追加

--- src/script/index.js
+++ src/script/index.js
@@ -8,21 +8,28 @@ const onClickAdd = () => {
     const btnComplete = $('<button>', {class: 'complete-button'}).html('完了');
     const btnDelete = $('<button>', {class: 'delete-button'}).html('削除');
 
-    const imcompleteList = div.append(li, btnComplete, btnDelete);
-    imcompleteList.appendTo('#imcomplete-list')    
+    const incompleteList = div.append(li, btnComplete, btnDelete);
+    incompleteList.appendTo('#incomplete-list');    
 
     // console.log(imcompleteList);
 }
 
 const onClickComplete = e => {
-    const deleteTarget = $(e.target).parent('.list-row');
-    deleteTarget.remove();
+    const moveTarget = $(e.target).parent('.list-row');
+    moveTarget.remove();
+    const text = moveTarget.children('li');
+    const btnBack = $('<button>', {class: 'toIncomplete-button'}).html('未完了に戻す');
+    const parent = moveTarget.empty();
+    const completeList = parent.append(text, btnBack);
+    console.log(text);
+    completeList.appendTo('#complete-list');
 }
 const onClickDelete = e => {
     const deleteTarget = $(e.target).parent('.list-row');
     deleteTarget.remove();
 }
 
+
 $('.add-button').click(onClickAdd);
-$('#imcomplete-list').click('.complete-button', onClickComplete);
-$('#imcomplete-list').click('.delete-button', onClickDelete);
+$('#incomplete-list').on('click', '.complete-button', onClickComplete);
+$('#incomplete-list').on('click', '.delete-button', onClickDelete);
